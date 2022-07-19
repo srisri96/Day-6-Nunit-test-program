ac7fc5f2489a1c57da770044615c6a5439623cc8
srisri96 <srikanthsitharth80@gmil.com>
2022-07-19 19:14:24 +0530

    Monthly payment

---
+++ MonthlyPayment/MonthlyPayment/Month.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthlyPayment
+{
+    internal class Month
+    {
+        public static double Test(double p, int y, double R)
+        {
+            int n = 12 * y;
+            double r = R / 1200;
+            return (p * r / (1 - (Math.Pow((1 + r), -n))));
+        }
+    }
+}
---
+++ MonthlyPayment/MonthlyPayment/Program.cs
@@ -0,0 +1,9 @@
+﻿using MonthlyPayment; 
+Console.WriteLine("Welcome to NUnit Testing program");
+Console.WriteLine("Enter the principle amount");
+double P = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter for how many years");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the rate to be compounded monthly");
+double r = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Payment to be made monthly is = {Month.Test(P, y, r)}");
