568e6b08e8d2c4326c7f1a2dd642b2acf1ecba7d
srisri96 <srikanthsitharth80@gmil.com>
2022-07-19 19:20:31 +0530

    Temprature

---
+++ TempratureConv/TempratureConv/Program.cs
@@ -0,0 +1,15 @@
+﻿using TempratureConv;
+Console.WriteLine("Welcome to NUnit Testing program");
+Console.WriteLine("Enter your choice : 1- Celsius to fahrenheit , 2- fahrenheit to Celsius");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+if (choice != 1 && choice != 2)
+{
+    Console.WriteLine("Wrong input");
+}
+else
+{
+    Console.WriteLine("Enter the value you want to convert");
+    double convert = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Converted value is = {Temp.Conv(choice, convert)}");
+}
---
+++ TempratureConv/TempratureConv/Temp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempratureConv
+{
+    internal class Temp
+    {
+        public static double Conv(int ch, double convert_into)
+        {
+            double converted = 0;
+            if (ch == 1)
+            {
+                converted = (convert_into * 9 / 5) + 32;              
+            }
+            else
+            {
+                converted = (convert_into - 32) * 5 / 9;                  
+            }
+            return converted;
+        }
+    }
+}
